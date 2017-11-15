85e4c78e274a0f875abc3d0b0411ac1e367db215
Russell Sabia <rjsabia@gmail.com>
2017-11-14 22:50:24 -0600

    added bin file, changed index js to app js, added random end point and id end point

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fortunes = require('./data/fortunes');
+
+const app = express();
+
+app.get('/fortunes', (req, res) => {
+	res.json(fortunes);
+});
+
+app.get('/fortunes/random', (req, res) => {
+	res.json(fortunes[Math.floor(Math.random() * fortunes.length)]);
+});
+
+app.get('/fortunes/:id', (req, res) => {
+	res.json(fortunes.find(f => f.id == req.params.id));
+});
+
+module.exports = app;
